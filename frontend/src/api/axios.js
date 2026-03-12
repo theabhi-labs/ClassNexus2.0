d71866bd11ce6a125649ca31a2202435e2e9bf9e
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-03-13 04:18:08 +0530

    add base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://class-nexus-api-w5d9.onrender.com", 
+  baseURL: "https://class-nexus-api-w5d9.onrender.com/api/v1", 
   withCredentials: true
 });
 
